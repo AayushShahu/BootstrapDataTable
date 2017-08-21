f2a746ed9afb03118c39ae5f7d6c882f24c502ac
Aayush Shahu <aayushs@cybage.com>
2017-08-21 20:01:05 +0530

    data Table commited

---
+++ details.sql
@@ -0,0 +1,41 @@
+/*
+SQLyog Community v12.4.3 (64 bit)
+MySQL - 5.7.17-log 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `details` (
+	`id` int (11),
+	`name` varchar (300),
+	`place` varchar (300),
+	`animal` varchar (300),
+	`thing` varchar (300)
+); 
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('1','aayush','nagpur','dog','mobile');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('2','saket','nagpur','cat','car');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('3','pratiksha','nashik','panda','clutch');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('4','abhijeet','pune','puppy','keys');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('5','Gokhale','nagpur','cow','paper');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('7','Sudeeksha','Nagpur','cat','Gum');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('8','Neha','Ranchi','Pikachu','Hum');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('9','Shruti','Aurangabad','Tiger','Pencil');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('24','jon','DC','cat','kitkat');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('25','tim','detroit','puppy','munch');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('26','john','nashville','cow','5star');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('27','casey','nagpur','bird','bat');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('28','jim','pune','penguin','ball');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('29','sam','aurangabad','ostrich','stump');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('30','braun','nashik','kiwi','book');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('31','roman','mumbai','ullu','page');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('32','kelly','indore','owl','pencil');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('33','romi','bhopal','hippo','rubber');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('34','rambo','varanasi','duck','eraser');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('35','james','jhansi','octopus','toy');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('36','justin','rajkot','fish','bike');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('37','sean','ahmedabad','salmon','car');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('38','brad','surat','crow','truck');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('39','emma','vadodara','toad','plane');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('40','angelina','luknow','frog','helicoptor');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('41','amanda','ajmer','aligator','ducati');
+insert into `details` (`id`, `name`, `place`, `animal`, `thing`) values('42','julian','jaipur','tortoise','audi');
